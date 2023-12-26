c437d60d95116e5d5db8d240fc28dfcceee0bb9d
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-26 15:28:59 +0700

    feat : complete Ex 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,15 @@ import axios from "axios";
 // http://localhost:4001/products
 function App() {
   const [productPost, setProductPost] = useState([]);
+  const [status, setStatus] = useState(0);
   const getProductPost = async () => {
-    const result = await axios.get("http://localhost:4001/products");
-    setProductPost(result.data.data);
+    try {
+      const result = await axios.get("http://localhost:4001/products");
+      setProductPost(result.data.data);
+      setStatus(1);
+    } catch (error) {
+      setStatus(2);
+    }
   };
   const deleteProductPost = async (id) => {
     await axios.delete(`http://localhost:4001/products/${id}`);
@@ -21,51 +27,37 @@ function App() {
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        {productPost.map((item) => {
-          return (
-            <div className="product" key={item.id}>
-              <div className="product-preview">
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  width="350"
-                  height="350"
-                />
-              </div>
-              <div className="product-detail">
-                <h1>Product name:{item.name}</h1>
-                <h2>Product price: {item.price} Baht</h2>
-                <p>Product description: {item.description}</p>
-              </div>
+        {status === 0 && <h1>Loading...</h1>}
+        {status === 1 &&
+          productPost.map((item) => {
+            return (
+              <div className="product" key={item.id}>
+                <div className="product-preview">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name:{item.name}</h1>
+                  <h2>Product price: {item.price} Baht</h2>
+                  <p>Product description: {item.description}</p>
+                </div>
 
-              <button
-                className="delete-button"
-                onClick={() => {
-                  deleteProductPost(item.id);
-                }}
-              >
-                x
-              </button>
-            </div>
-          );
-        })}
-        {/* <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
-        </div> */}
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteProductPost(item.id);
+                  }}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        {status === 2 && <h1>Fetching Error...</h1>}
       </div>
     </div>
   );
